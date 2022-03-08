bce34d0755b3eef68d95a17a2759529c44d95270
HadToRegister <rhesuspond@gmail.com>
2022-03-08 15:40:30 -0500

    Update

---
+++ ReactorVolume/ReactorVolume.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using Logger = QModManager.Utility.Logger;
+
+namespace ReactorVolume
+{
+    [HarmonyPatch(typeof(BaseNuclearReactorGeometry), nameof(BaseNuclearReactorGeometry.SetState))]
+    internal class ReactorVolume
+    {
+        [HarmonyPostfix]
+        public static void Postfix(BaseNuclearReactorGeometry __instance, bool state)
+        {
+            // Turn off reactor sound
+            if (state)
+            {
+                __instance.workSound.Stop();
+            }
+        }
+    }
+}
